7c05810e93ab8e546338d3a7867e3c4c994cec13
imyogeshgaur <yogeshgaur1995@gmail.com>
2023-01-19 10:05:19 +0530

    Basics of dotnet

---
+++ firstProject/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using firstProject.Models;
+
+
+namespace firstProject.Controllers;
+
+public class HomeController : Controller
+{
+    public string Name()
+    {
+        return "My name is Yogesh";
+    }
+
+    public string EmployeeProfile(int id){
+        if(id==1){
+            return  "Employee 1 Data";
+        }else{
+            return "No Data ";
+        }
+    }
+
+    [HttpGet]
+    public ActionResult Index()
+    {
+        return View();
+    }
+
+    public ViewResult Privacy(){
+        return View();
+    }
+    public ViewResult About(){
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Form()
+    {
+        
+        return View();
+    }
+
+    public ActionResult Employee(){
+        var data = GetEmployeeData();
+        return View(data);
+    }
+
+    public Employee GetEmployeeData(){
+        return new Employee()
+        {
+            employeeId=101,
+            employeeName="Ramesh",
+            employeeSalary = 1001.322F
+        };
+    }
+}
---
+++ firstProject/Models/Employee.cs
@@ -0,0 +1,9 @@
+namespace firstProject.Models;
+
+public class Employee
+{
+    public int employeeId {get;set;}
+    public string ? employeeName {get;set;}
+
+    public float employeeSalary {get;set;}
+}
