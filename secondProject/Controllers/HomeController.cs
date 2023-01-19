480e33898faebe3b24e7d354b6dd274738eecfc7
imyogeshgaur <yogeshgaur1995@gmail.com>
2023-01-19 14:22:19 +0530

    Crud operaions

---
+++ secondProject/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using secondProject.Models;
+
+namespace secondProject.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult Register()
+    {
+        
+        return View();
+    }
+    
+    [HttpPost]
+
+    public string PostData(){
+        string firstName = Request.Form["firstName"];
+        string middleName = Request.Form["middleName"];
+        string lastName = Request.Form["lastName"];
+
+       return "First Name is : "+ firstName +" Middle Name is : " + middleName +" Last Name is : " + lastName;
+    }
+}
